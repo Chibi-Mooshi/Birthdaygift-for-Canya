e7f78f00eb6e7eee1a92aeda381c37bad57ad65f
ChibiMooshi <70508345+ChibiMooshi@users.noreply.github.com>
2021-08-26 21:40:52 +0200

    added companion

---
+++ Canya's gift/Assets/NPCs/Scripts/ChoiceHolderScriptableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public enum Companion { Warrior, Druid, Alone };
+[CreateAssetMenu(menuName ="Companion", fileName ="Companion")]
+public class ChoiceHolderScriptableObject : ScriptableObject
+{
+
+
+}
